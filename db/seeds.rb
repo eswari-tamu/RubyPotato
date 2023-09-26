8498d7606f06bbe6be12cad7526306a3441c043a
eswari-tamu <eswari.choudary@tamu.edu>
2023-09-26 00:27:10 -0500

    Update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,10 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#   Character.create(name: "Luke", movie: movies.first)
 # Seed the RottenPotatoes DB with some movies.
 more_movies = [
   {:title => 'My Neighbor Totoro', :rating => 'G',
@@ -9,9 +16,9 @@ more_movies = [
   {:title => 'Nomadland', :rating => 'R',
     :release_date => '19-Feb-2021'},
   {:title => 'CODA', :rating => 'PG-13',
-    :release_date => '13-Aug-2021'}
+    :release_date => '13-Aug-2021'},
   {:title => 'Now you see me', :rating => 'G',
-    :release_date => '09-Aug-2013'},
+    :release_date => '10-Aug-2013'},
   {:title => 'Marakanta Mani', :rating => 'G',
     :release_date => '16-Jun-2017'}
 ]
